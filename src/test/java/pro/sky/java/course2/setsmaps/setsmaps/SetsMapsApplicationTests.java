9330a305f9f5c62670e89c6590164538119625cd
Admin <anstdor@gmail.com>
2023-04-16 06:11:37 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/setsmaps/setsmaps/SetsMapsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.setsmaps.setsmaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetsMapsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
